d7b79912baf3c05e44a71292e91ebcadcaf413df
Kevin Ahlstrom <ikeavin@gmail.com>
2021-02-24 18:29:23 -0700

    dak

--- cli.c
+++ cli.c
@@ -6,8 +6,34 @@
 #include <sys/wait.h>
 #include <string.h>
 
+//Created by Kevin Ahlstrom for CST 315
+//All work within is my own work
+//Sources: 
+//learnc.org
+//Effective C by Robert Seacord
+
+void cd(char* dir, char** parseDir) {
+	int i;
+	for(i = 0; i < 100; i++) {
+		parseDir[i] = strsep(&dir, " ");
+		if(parseDir[i] == NULL) {
+			break;
+		}
+		if(strlen(parseDir[i]) == 0) {
+			i--;
+		}
+	}
+	chdir(parseDir[1]);
+}
+
 void execute(char *input, int len) {
-	system(input);
+	printf("%c", input[0]);
+	if(input[0] == 'c' && input[1] == 'd') {
+		char* parseDir[100];
+		cd(input, parseDir);
+	} else {
+		system(input);
+	}
 }
 
 int run_cli(void) {
@@ -19,11 +45,14 @@ int run_cli(void) {
 		ssize_t lineSize = 0;
 		printf("kevOS: ");
 		lineSize = getline(&in, &len, stdin);
-	
 		//Exit if 'Ctrl-X' is pressed
 		if(in[0] == 24) {
 			exit(0);
 			return 0;
+		} else if(in[0] == 'c' && in[1] == 'd') {
+			char* parseDir[100];
+			cd(in, parseDir);
+			continue;
 		}
 
 		//Otherwise create a fork and wait for fork to complete
